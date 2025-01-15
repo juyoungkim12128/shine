ce5b0e1bdc6ce587bdea308ece615c2c7782abc0
juyoung <juyoung12128@gmail.com>
2025-01-15 13:31:13 +0000

    apply changes

--- create_table.sql
+++ create_table.sql
@@ -14,22 +14,23 @@ CREATE TABLE post (
     userId VARCHAR(100) NOT NULL,                    
     postTitle VARCHAR(255) NOT NULL,        
     postContent TEXT NOT NULL,
-    sFilename VARCHAR(255), --저장될 파일명
-    oFilename VARCHAR(255), --실제 파일명
+    sFilename VARCHAR(255),
+    oFilename VARCHAR(255),
     createdTime DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,        
     lastModifiedTime DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (userId) REFERENCES user(userId) ON DELETE CASCADE   
 );
 
 CREATE TABLE comment (
-  comment_id INT(100) PRIMARY KEY AUTO_INCREMENT,
-  user_id INT(100) NOT NULL,
+  comment_id INT PRIMARY KEY AUTO_INCREMENT,
+  user_id VARCHAR(100) NOT NULL,
   post_id INT NOT NULL,
   content_text VARCHAR(255) NOT NULL,
   createdTime DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,        
   lastModifiedTime DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (userId) REFERENCES user(userId) ON DELETE CASCADE  
-  FOREIGN KEY (postId) REFERENCES post(postId) ON DELETE CASCADE
+  FOREIGN KEY (user_id) REFERENCES user(userId) ON DELETE CASCADE,  
+  FOREIGN KEY (post_id) REFERENCES post(postId) ON DELETE CASCADE
 );
+
 
 
